0bc7a33b0eecfb1842bc809d5ea6d11ca8f1962a
aliirz <dejavu87@gmail.com>
2015-04-24 15:10:41 +0500

    Added .gitignore and code clean up

--- CordovaSource/platforms/android/CordovaLib/ant-build/AndroidManifest.xml.d
+++
@@ -1,2 +0,0 @@
-D:\Node\Cordova\sample applications\sps\platforms\android\CordovaLib\ant-build\AndroidManifest.xml : \
-D:\Node\Cordova\sample applications\sps\platforms\android\CordovaLib\AndroidManifest.xml \
--- CordovaSource/platforms/android/ant-build/AndroidManifest.xml.d
+++
@@ -1,2 +0,0 @@
-D:\Node\Cordova\sample applications\sps\platforms\android\ant-build\AndroidManifest.xml : \
-D:\Node\Cordova\sample applications\sps\platforms\android\AndroidManifest.xml \
