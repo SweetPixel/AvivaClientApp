5a947dcb81d81510d45e5b2ec6f2cec31c207dd6
Ali Raza <ali@sweetpixelstudios.com>
2015-07-03 14:12:16 +0500

    Addresses #7: CSS, JS Minification

--- gulpfile.js
+++ gulpfile.js
@@ -2,11 +2,33 @@
 
 
 var gulp        = require('gulp'),
-    connect        = require('gulp-connect');
+	useref		= require('gulp-useref'),
+	gulpif		= require('gulp-if'),
+	uglify		= require('gulp-uglify'),
+	minifyCss	= require('gulp-minify-css'),
+	googleWebFonts = require('gulp-google-webfonts'),
+    connect     = require('gulp-connect');
 
 
 gulp.task('default', function () {
-  
+	var assets = useref.assets();
+
+	return gulp.src('www/*.html')
+		.pipe(assets)
+		.pipe(gulpif('*.js', uglify()))
+		.pipe(gulpif('*.css', minifyCss()))
+		.pipe(assets.restore())
+		.pipe(useref())
+		.pipe(gulp.dest('dist'));
+
+});
+
+
+gulp.task('fonts', function () {
+	return gulp.src('fonts.list')
+		.pipe(googleWebFonts())
+		.pipe(gulp.dest('www/font'))
+		;
 });
 
 
