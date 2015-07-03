73e41e315abd8a22cc78151363b87417be13c920
Muhammad Sayyam <sayyam@sweetpixelstudios.com>
2015-07-03 12:22:41 +0500

    npm, bower, gulp installation

---
+++ gulpfile.js
@@ -0,0 +1,18 @@
+'use strict';
+
+
+var gulp        = require('gulp'),
+    connect        = require('gulp-connect');
+
+
+gulp.task('default', function () {
+  
+});
+
+
+gulp.task('server', function () {
+    connect.server({
+        root: 'www',
+        livereload: true
+    });
+});
