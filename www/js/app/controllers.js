423b4666f3c73bdeb01c38a4d58ffe098657d728
Muhammad Sayyam <sayyam@sweetpixelstudios.com>
2015-10-06 12:41:03 +0500

    created feedback controller and service for medical, dental and optical

--- www/js/app/controllers.js
+++ www/js/app/controllers.js
@@ -89,6 +89,15 @@ avivaApp.controller('familyDetailsCtrl', function ($log, $http, $scope, $locatio
 		
 	}
 })
+avivaApp.controller('feedbackCtrl', function($http, $scope, feedbackService) {
+	$scope.feedback = {};
+	console.log("getting feedback: " + $scope.$parent.service);
+	$scope.promise = feedbackService.getFeedbacks($scope.$parent.userId, $scope.$parent.service);
+	$scope.promise.then(function (payload) {
+		console.log("Got feedback");
+		$scope.feedback = payload.feedback;
+	})
+})
 avivaApp.controller('findDentistCtrl', function($scope, $http, mapService, $log, $location){
 	$scope.clinics = [];
 	$scope.sortBy = '+distance';
--- www/js/app/services.js
+++ www/js/app/services.js
@@ -79,6 +79,36 @@ avivaApp.factory('familyDetailsService', function ($http, $q, $log) {
 		}
 	}
 })
+avivaApp.factory('feedbackService', function ($http, $q) {
+	return {
+		getFeedbacks: function (userId, service) {
+			var deferred = $q.defer();
+			var url = '';
+			switch (service) {
+				case 1:
+				url = 'https://dentalink.co.uk/healthpickapi/api/DentalService/Dental?username=' + userId;
+				break;
+				case 2:
+				url = 'https://dentalink.co.uk/healthpickapi/api/MedicalService/Medical?username=' + userId;
+				break;
+				case 3:
+				url = 'https://dentalink.co.uk/healthpickapi/api/OpticalService/Optical?username=' + userId;
+				break;
+			}
+			$http.get(url)
+			.success(function (response) {
+				deferred.resolve({
+					feedback: response
+				})
+			})
+			.error(function (msg, code) {
+				deferred.reject(msg);
+				$log.error(msg, code);
+			});
+			return deferred.promise;
+		}
+	}
+})
 avivaApp.factory('getPersonalService', function ($http, $q, $log) {
 	return {
 		getDetails: function (userId) {
