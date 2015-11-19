3b346159878fd3c845dcc83bbe93641c3c35036c
Nauroz Ahmad <thenewdawn1994@hotmail.com>
2015-11-19 19:50:46 +0500

    removed progressindicator plugin

--- www/js/app/controllers/login.ctrl.js
+++ www/js/app/controllers/login.ctrl.js
@@ -5,15 +5,17 @@ avivaApp.controller('loginCtrl', function ($scope, $location, loginService) {
 	}
 	$scope.status = '';
 	$scope.login = function () {
-		ProgressIndicator.showSimple(true);
+		// ProgressIndicator.showSimple(true);
 		$scope.ASyncStarted = true;
 		$scope.promise = loginService.login($scope.credentials);
+		console.log("Logging in");
 		$scope.promise.then(function (payload) {
 			$scope.status = payload.status;
 			$scope.ASyncStarted = false;
 			$scope.$parent.userId = $scope.credentials.username;
 			$scope.$parent.checkNotifications();
-			ProgressIndicator.hide();
+			console.log("received");
+			// ProgressIndicator.hide();
 			if($scope.status.Status == true) {
 				$location.path('/services');
 			}
